8f08498c71ba39fe6f4c8cfafdc1aca5b925e678
CYTAdmin <chooseyourtherapist@gmail.com>
2026-03-13 17:19:26 -0700

    setting

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import app from "./app.js";
-
-dotenv.config({ path: "./.env" });
-mongoose.set("strictQuery", true);
-
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-      console.log(`✅ Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+import dotenv from \"dotenv\";  
+dotenv.config({ path: \"./.env\" });  
+import mongoose from \"mongoose\";  
+import app from \"./app.js\";  
+mongoose.set(\"strictQuery\", true);  
+mongoose.connect(process.env.MONGODB_URI).then(() => {  
+  const PORT = process.env.PORT || 4000;  
+  app.listen(PORT, () => {  
+    console.log(\"Server running\");  
+  });  
+}).catch((err) => console.log(err)); 
